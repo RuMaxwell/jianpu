e7f20bbdd62e5cb8a22a17103258732d5a3a0d99
RuMaxwell <25785855+RuMaxwell@users.noreply.github.com>
2025-05-02 15:04:09 +0900

    chore: Add repository to package.json and the symbol to be used in code.

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from '@rsbuild/core'
 import { pluginReact } from '@rsbuild/plugin-react'
-import { version } from './package.json'
+import { version, repository } from './package.json'
 
 export default defineConfig({
   plugins: [pluginReact()],
   source: {
     define: {
       PACKAGE_VERSION: JSON.stringify(version),
+      PACKAGE_REPOSITORY: JSON.stringify(repository),
     },
   },
 })
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="@rsbuild/core/types" />
 
 declare const PACKAGE_VERSION: string
+declare const PACKAGE_REPOSITORY: string
